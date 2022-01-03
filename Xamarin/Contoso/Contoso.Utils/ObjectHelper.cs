c2a84fabb103c70d5d8ce8ac89f049c6fe2b5e3b
Blaise Taylor <btaylor@bPolicySolutions.com>
2022-01-03 13:33:50 -0500

    minor

--- Xamarin/Contoso/Contoso.Utils/ObjectHelper.cs
+++ Xamarin/Contoso/Contoso.Utils/ObjectHelper.cs
@@ -1,9 +1,15 @@
-﻿namespace Contoso.Utils
+﻿using LogicBuilder.Attributes;
+
+namespace Contoso.Utils
 {
     public static class ObjectHelper
     {
         public static object Null = null;
 
+        [AlsoKnownAs("ObjectIsNull")]
         public static bool IsNull(object anyObject) => anyObject == null;
+
+        [AlsoKnownAs("StringIsNullOrEmpty")]
+        public static bool StringIsNullOrEmpty(string value) => string.IsNullOrEmpty(value);
     }
 }
