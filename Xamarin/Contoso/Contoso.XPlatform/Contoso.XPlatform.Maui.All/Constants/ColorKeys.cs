561911c113422e0cab4fd763c4aed1905a7df863
Blaise Taylor <btaylor@bPolicySolutions.com>
2022-09-08 15:32:56 -0400

    Arranging theme colors alphabetically and adding keys to ColorKeys struct.

--- Xamarin/Contoso/Contoso.XPlatform/Contoso.XPlatform.Maui.All/Constants/ColorKeys.cs
+++ Xamarin/Contoso/Contoso.XPlatform/Contoso.XPlatform.Maui.All/Constants/ColorKeys.cs
@@ -2,6 +2,30 @@
 {
     internal struct ColorKeys
     {
+        public const string CheckBoxCheckedColor = nameof(CheckBoxCheckedColor);
+        public const string CheckBoxColor = nameof(CheckBoxColor);
+        public const string CheckBoxDisabledColor = nameof(CheckBoxDisabledColor);
+        public const string CommandBarBackgroundColor = nameof(CommandBarBackgroundColor);
+        public const string CommandButtonBackgroundColor = nameof(CommandButtonBackgroundColor);
+        public const string CommandButtonBorderColor = nameof(CommandButtonBorderColor);
+        public const string EntryBackgroundColor = nameof(EntryBackgroundColor);
+        public const string ErrorTextColor = nameof(ErrorTextColor);
+        public const string HyperLinkColor = nameof(HyperLinkColor);
+        public const string NavigationBarColor = nameof(NavigationBarColor);
+        public const string PageBackgroundColor = nameof(PageBackgroundColor);
         public const string PopupViewBackgroundColor = nameof(PopupViewBackgroundColor);
+        public const string PressedPrimaryColor = nameof(PressedPrimaryColor);
+        public const string PrimaryColor = nameof(PrimaryColor);
+        public const string PrimaryTextColor = nameof(PrimaryTextColor);
+        public const string ResultListBackgroundColor = nameof(ResultListBackgroundColor);
+        public const string ResultListBorderColor = nameof(ResultListBorderColor);
+        public const string SearchBarDisabledTextColor = nameof(SearchBarDisabledTextColor);
+        public const string SecondaryColor = nameof(SecondaryColor);
+        public const string SecondaryTextColor = nameof(SecondaryTextColor);
+        public const string SelectedCommandButtonBackgroundColor = nameof(SelectedCommandButtonBackgroundColor);
+        public const string SwitchOnColor = nameof(SwitchOnColor);
+        public const string SwitchThumbColor = nameof(SwitchThumbColor);
+        public const string TertiaryTextColor = nameof(TertiaryTextColor);
+        public const string TransparentColor = nameof(TransparentColor);
     }
 }
