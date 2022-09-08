85dc3147a4c2b01e57c4fa4ae77de71d27a46dc9
Blaise Taylor <btaylor@bPolicySolutions.com>
2022-09-08 15:04:13 -0400

    Grouping styles by target types and adding style keys to struct.

--- Xamarin/Contoso/Contoso.XPlatform/Contoso.XPlatform.Maui.All/Constants/StyleKeys.cs
+++ Xamarin/Contoso/Contoso.XPlatform/Contoso.XPlatform.Maui.All/Constants/StyleKeys.cs
@@ -2,12 +2,35 @@
 {
     internal struct StyleKeys
     {
+        public const string BaseEntryStyle = nameof(BaseEntryStyle);
+        public const string BodyTextStyle = nameof(BodyTextStyle);
+
+        public const string ChildFormPopupScrollViewStyle = nameof(ChildFormPopupScrollViewStyle);
+        public const string ChildFormPopupViewStyle = nameof(ChildFormPopupViewStyle);
+
+        public const string DetailFormDeleteQuestionStyle = nameof(DetailFormDeleteQuestionStyle);
+        public const string DetailFormEntryStyle = nameof(DetailFormEntryStyle);
+        public const string DetailFormGroupHeaderStyle = nameof(DetailFormGroupHeaderStyle);
+        public const string DetailFormLabel = nameof(DetailFormLabel);
+
+        public const string EditFormGroupHeaderStyle = nameof(EditFormGroupHeaderStyle);
+        public const string EditFormLabel = nameof(EditFormLabel);
+
         public const string FormArrayPopupCollectionViewStyle = nameof(FormArrayPopupCollectionViewStyle);
         public const string FormArrayPopupViewStyle = nameof(FormArrayPopupViewStyle);
+
         public const string HeaderStyle = nameof(HeaderStyle);
         public const string HeaderTextDetailListItemStyle = nameof(HeaderTextDetailListItemStyle);
         public const string HeaderTextDetailItemLayout = nameof(HeaderTextDetailItemLayout);
+
+        public const string ListFormCollectionViewStyle = nameof(ListFormCollectionViewStyle);
+        public const string ListItemStyle = nameof(ListItemStyle);
+        public const string ListItemTextStyle = nameof(ListItemTextStyle);
+
+        public const string MultiSelectItemStyle = nameof(MultiSelectItemStyle);
+        public const string MultiSelectPopupCollectionViewStyle = nameof(MultiSelectPopupCollectionViewStyle);
         public const string MultiSelectPopupViewStyle = nameof(MultiSelectPopupViewStyle);
+
         public const string PopupAcceptButtonStyle = nameof(PopupAcceptButtonStyle);
         public const string PopupAddButtonStyle = nameof(PopupAddButtonStyle);
         public const string PopupCancelButtonStyle = nameof(PopupCancelButtonStyle);
@@ -20,9 +43,18 @@
         public const string PopupHeaderLabelStyle = nameof(PopupHeaderLabelStyle);
         public const string PopupHeaderStyle = nameof(PopupHeaderStyle);
         public const string PullButtonStyle = nameof(PullButtonStyle);
+
         public const string SearchFormCollectionViewStyle = nameof(SearchFormCollectionViewStyle);
         public const string SearchFormRefreshViewStyle = nameof(SearchFormRefreshViewStyle);
+        public const string SubtitleTextStyle = nameof(SubtitleTextStyle);
+
         public const string TextDetailItemLayout = nameof(TextDetailItemLayout);
         public const string TextDetailListItemStyle = nameof(TextDetailListItemStyle);
+        public const string TextFormGroupHeaderStyle = nameof(TextFormGroupHeaderStyle);
+        public const string TextFormHyperLinkLabelStyle = nameof(TextFormHyperLinkLabelStyle);
+        public const string TextFormHyperLinkSpanStyle = nameof(TextFormHyperLinkSpanStyle);
+        public const string TextFormItemLabelStyle = nameof(TextFormItemLabelStyle);
+        public const string TextFormItemSpanStyle = nameof(TextFormItemSpanStyle);
+        public const string TitleTextStyle = nameof(TitleTextStyle);
     }
 }
